5b132f79ed0c69eed55e9b45e3f342d41a3de4c4
Péter Molnár <xodax9898@gmail.com>
2021-03-16 15:49:39 +0100

    From 2nd task the 2nd and 3rd subtasks are finished
    In the 2nd subtask I have used HashSet because in this way I don't have to worry about duplicates.
    
    In the 3rd subtask I have used Dictionary because It was the most simple solution due to the key-value pairs.

---
+++ 2nd task/2ndTask/2ndTask/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2ndTask
+{
+    class Program
+    {
+        static Random rn = new Random();
+        public static Dictionary<string, string> olvassTxt = new Dictionary<string, string>();
+
+        static HashSet<int> rnList(int db, int end)
+        {
+            var results = new HashSet<int>();
+            while (results.Count < db)
+            {
+                results.Add(rn.Next(1, end));
+            }
+
+            return results;
+        } // I have used HashSet because in this way I don't have to worry about duplicates because it removes them.
+
+        static void readTxt(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            string row;
+            string key;
+            string value;
+            while ((row = sr.ReadLine()) != null)
+            {
+                string[] data = row.Split('|');
+                key = data[0];
+                value = data[1];
+                olvassTxt.Add(key, value);
+            }
+            sr.Close();
+        }
+
+            static void Main(string[] args)
+        {
+            var list = rnList(10, 50);
+            readTxt("olvass.txt");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
