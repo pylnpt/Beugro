ba5bac2a4153cb2c438adcd980a16ee85a888268
Péter Molnár <xodax9898@gmail.com>
2021-03-18 01:15:05 +0100

    I have finished the second task.
    You may see in the "results.txt" file some questionmarks instead of the accented characters such as áéí etcetc. The reason behind this problem is that the encoding of the import.sql .

--- 2nd task/2ndTask/2ndTask/Program.cs
+++ 2nd task/2ndTask/2ndTask/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using MySql.Data.MySqlClient;
 
 namespace _2ndTask
 {
@@ -11,6 +12,9 @@ namespace _2ndTask
     {
         static Random rn = new Random();
         public static Dictionary<int, string> olvassTxt = new Dictionary<int, string>();
+        public const string connectionString = "server=localhost; port=3308; user id=root; password=; database=cs_beugro; CharSet=utf8;";
+        static string fileName = @"C:\Users\Admin\Documents\GitHub\Beugro\2nd task\2ndTask\2ndTask\results.txt";
+        static StreamWriter sw = new StreamWriter((new FileStream(fileName, FileMode.Open)),  Encoding.UTF8);
 
         static HashSet<int> rnList(int db, int end)
         {
@@ -41,7 +45,6 @@ namespace _2ndTask
             }
             sr.Close();
         }
-
         static List<string> searchedValues(HashSet<int> nums, Dictionary<int,string> dict)
         {
             List<string> results = new List<string>();
@@ -67,13 +70,27 @@ namespace _2ndTask
             var list = rnList(10, 50);
             readTxt("olvass.txt");
             List<string>user_id_list = searchedValues(list, olvassTxt);
-            foreach (var item in user_id_list)
-            {
-                Console.WriteLine(item);
-            }
-
             
-
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            
+            sw.WriteLine("Tulajdonos" + "\t" + "Márka" + "\t" + "Típus");
+            for (int i = 0; i < user_id_list.Count; i++)
+            {
+                MySqlCommand cmd_database = new MySqlCommand(String.Format("SELECT user.name, car.brand, car.model FROM user, car, user_car WHERE user.id = user_car.user AND user_car.car = car.id AND user.id = {0}", user_id_list[i]), connection);
+                MySqlDataReader dr = cmd_database.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                                sw.WriteLine(dr.GetString(0) + "\t" + dr.GetString(1) + "\t" + dr.GetString(2));
+                                Console.WriteLine(dr.GetString(0) + "\t" + dr.GetString(1) + "\t" + dr.GetString(2));
+                        
+                    }
+                }
+                dr.Close();
+            }
+            sw.Close();
             Console.ReadKey();
         }
     }
